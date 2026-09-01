ad70f486ebfed30316926ff151d804f8be0a9002
Erik Simonsson <erik.simonsson@knightec.se>
2026-09-01 16:49:14 +0200

    Added number pad

--- main.cpp
+++ main.cpp
@@ -17,9 +17,25 @@ int main(int argc, char **argv) {
 
     auto *oneButton = new QPushButton("1");
 
+    auto *keypadLayout = new QGridLayout();
+
+    keypadLayout->addWidget(new QPushButton("7"), 0, 0);
+    keypadLayout->addWidget(new QPushButton("8"), 0, 1);
+    keypadLayout->addWidget(new QPushButton("9"), 0, 2);
+
+    keypadLayout->addWidget(new QPushButton("4"), 1, 0);
+    keypadLayout->addWidget(new QPushButton("5"), 1, 1);
+    keypadLayout->addWidget(new QPushButton("6"), 1, 2);
+
+    keypadLayout->addWidget(oneButton, 2, 0);
+    keypadLayout->addWidget(new QPushButton("2"), 2, 1);
+    keypadLayout->addWidget(new QPushButton("3"), 2, 2);
+
+    keypadLayout->addWidget(new QPushButton("0"), 3, 1);
+
     auto *layout = new QVBoxLayout(&window);
     layout->addWidget(display);
-    layout->addWidget(oneButton);
+    layout->addLayout(keypadLayout);
 
     QObject::connect(oneButton, &QPushButton::clicked, [display]() {
         display->setText("1");
